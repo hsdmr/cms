40313e661997975e3373df9d34ee040e3accd297
hsdmr <hsdmrsoft@gmail.com>
2021-11-12 03:30:11 +0300

    removed unused methods

--- models/Post.php
+++ models/Post.php
@@ -4,7 +4,6 @@ namespace Hasdemir\Model;
 
 use Hasdemir\Base\Model;
 use Hasdemir\Base\System;
-use Hasdemir\Exception\DefaultException;
 
 class Post extends Model
 {
@@ -13,34 +12,13 @@ class Post extends Model
     protected const PRIMARY_KEY = 'id';
     protected const FIELDS = ['permalink_id', 'user_id', 'file_id', 'status', 'title', 'content'];
     protected const UNIQUES = ['permalink_id'];
+    protected const HIDDENS = ['deleted_at', 'created_at', 'update_at'];
     protected const SOFT_DELETE = true;
     
     public function __construct(\PDO $db = null)
     {
         $this->db = $db ?? System::get('pdo');
-        parent::__construct($this->db, self::TABLE, self::PRIMARY_KEY, self::FIELDS, self::UNIQUES, self::SOFT_DELETE);
+        parent::__construct($this->db, self::TABLE, self::PRIMARY_KEY, self::FIELDS, self::UNIQUES, self::HIDDENS, self::SOFT_DELETE);
     }
 
-    public function create($data)
-    {
-        throw new DefaultException('deneme');
-        return $this->insert([
-            'permalink_id' => $data['permalink_id'],
-            'user_id' => $data['user_id'] ?? 1,
-            'file_id' => $data['file_id'] ?? null,
-            'status' => $data['status'] ?? 'published',
-            'title' => $data['title'] ?? 'Post_'. uniqid(),
-            'content' => $data['content'] ?? '',
-        ]);
-    }
-    
-    public function update($data)
-    {
-        return $this->set([
-            'file_id' => $data['file_id'] ?? null,
-            'status' => $data['status'] ?? 'published',
-            'title' => $data['title'] ?? 'Post_'. uniqid(),
-            'content' => $data['content'] ?? '',
-        ]);
-    }
 }
