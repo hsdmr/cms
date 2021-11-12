edfec5bd5e7d2f05f6ba4ebda4666737df9176aa
hsdmr <hsdmrsoft@gmail.com>
2021-11-12 03:27:44 +0300

    improved logs

--- base/Log.php
+++ base/Log.php
@@ -36,21 +36,32 @@ class Log
     public static function currentJob($job)
     {
         self::$currentJob = $job;
-        $log = '[' . date('Y-m-d H:i:s') . '] ' . self::$currentJob . ' started.' . PHP_EOL;
+        $log = '[' . date('Y-m-d H:i:s') . '] Start => \'' . self::$currentJob . '\'' . PHP_EOL;
         self::insert($log, 'daily');
     }
     
     public static function endJob()
     {
-        $log = '[' . date('Y-m-d H:i:s') . '] ' . self::$currentJob . ' ended.' . PHP_EOL;
+        $log = '[' . date('Y-m-d H:i:s') . '] End => \'' . self::$currentJob . '\'' . PHP_EOL;
+        self::insert($log, 'daily');
+    }
+    
+    public static function request($url, $method)
+    {
+        $log = '[' . date('Y-m-d H:i:s') . '] Request url => \'' . $url . '\', method => \'' . $method . '\'' . PHP_EOL;
         self::insert($log, 'daily');
     }
 
     public static function error($response, $e, $th)
     {
-        $log = '[' . date('Y-m-d H:i:s') . '] Throwed error. Message => \'' . $response['message'] . '\', Status Code => \'' . $e->http_code . '\'' . PHP_EOL;
+        $log = '[' . date('Y-m-d H:i:s') . '] Throwed error. Message => "' . $response['message'] . '", Status Code => \'' . $e->http_code . '\'' . PHP_EOL;
         self::insert($log, 'daily');
-        $log = '[' . date('Y-m-d H:i:s') . '] {"message": "' . $e->getMessage() . '", "status": "' . $e->http_code . '", "file": "' . $e->getFile() . '", "line": "' . $e->getLine() . '"}' . PHP_EOL . PHP_EOL;
+        $log = '[' . date('Y-m-d H:i:s') . '] {"message": "' . $e->getMessage() . '", "status": "' . $e->http_code . '", "file": "' . $e->getFile() . '", "line": "' . $e->getLine() . '"}';
+        /*foreach ($th as $t) {
+            $log .= '{"message": "' . $t->getMessage() . '", "status": "' . $t->http_code . '", "file": "' . $t->getFile() . '", "line": "' . $t->getLine() . '"}';
+        }*/
+        $log .= PHP_EOL . PHP_EOL;
+
         self::insert($log, 'error', false);
     }
 }
