b565ad376ddde48c984a84d9ca4e52eb7c7abf65
hsdmr <hsdmrsoft@gmail.com>
2021-11-13 02:38:59 +0300

    added function

---
+++ base/helper/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('random_string')) {
+    function random_string(int $length = 60): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $random_string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $random_string .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $random_string;
+    }
+}
