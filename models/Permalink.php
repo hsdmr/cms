74475a405527853afd58b35922da048b3866fc77
hsdmr <hsdmrsoft@gmail.com>
2021-11-21 01:54:37 +0300

    added Permalink model

---
+++ models/Permalink.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Hasdemir\Model;
+
+use Hasdemir\Base\Model;
+
+class Permalink extends Model
+{
+    protected $table = 'permalink';
+    protected $unique = ['path'];
+    protected $soft_delete = true;
+
+    public static function findById(int $id)
+    {
+        $item = new User();
+        return $item->find($id);
+    }
+
+    public static function findByPath(string $path)
+    {
+        $item = new User();
+        return $item->where([['path', '=', $path]])->first();
+    }
+
+}
