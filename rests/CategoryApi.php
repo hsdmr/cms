d1a15cbc6523b1b48a90e44c5e6de33cf2b0fbee
hsdmr <hsdmrsoft@gmail.com>
2021-11-21 16:30:21 +0300

    fix nullType

--- rests/CategoryApi.php
+++ rests/CategoryApi.php
@@ -32,10 +32,10 @@ class CategoryApi extends BaseApi
             $this->validate($_POST);
             $category = new Category();
             $this->body = $category->create([
-                'permalink_id' => $_POST['permalin_id'],
+                'permalink_id' => $_POST['permalink_id'],
                 'file_id' => $_POST['file_id'] ?? null,
                 'parent_id' => $_POST['parent_id'] ?? null,
-                'type' => $_POST['type'],
+                'owner' => $_POST['owner'],
                 'title' => $_POST['title'] ?? 'Category-' . uniqid(),
                 'content' => $_POST['content'] ?? '',
             ])->toArray();
@@ -73,10 +73,10 @@ class CategoryApi extends BaseApi
             
             $category = Category::findById($category_id);
             $this->body = $category->update([
-                'permalink_id' => $_PUT['permalin_id'],
+                'permalink_id' => $_PUT['permalink_id'],
                 'file_id' => $_PUT['file_id'] ?? null,
                 'parent_id' => $_PUT['parent_id'] ?? null,
-                'type' => $_PUT['type'],
+                'owner' => $_PUT['owner'],
                 'title' => $_PUT['title'] ?? 'Category-' . uniqid(),
                 'content' => $_PUT['content'] ?? '',
             ])->toArray();
@@ -105,13 +105,14 @@ class CategoryApi extends BaseApi
         if (!v::key('permalink_id', v::positive())->validate($params)) {
             throw new UnexpectedValueException("'permalink_id' must be positive number", self::HELPER_LINK);
         }
-
-        if (!v::key('type', v::positive())->validate($params)) {
-            throw new UnexpectedValueException("'user_id'  must be positive number", self::HELPER_LINK);
+        if (!v::key('owner', v::in(['post', 'product', 'lesson']))->validate($params)) {
+            throw new UnexpectedValueException("'owner' must be 'post', 'product', 'lesson'", self::HELPER_LINK);
         }
-        
-        if (!v::key('type', v::in(['post', 'product', 'tutor']))->validate($params)) {
-            throw new UnexpectedValueException("'status' only can be 'published' or 'draft'", self::HELPER_LINK);
+        if (!v::key('file_id', v::anyOf(v::nullType(), v::positive()), false)->validate($params)) {
+            throw new UnexpectedValueException("'file_id' must be positive number", self::HELPER_LINK);
+        }
+        if (!v::key('parent_id', v::anyOf(v::nullType(), v::positive()), false)->validate($params)) {
+            throw new UnexpectedValueException("'parent_id' must be positive number", self::HELPER_LINK);
         }
     }
 }
